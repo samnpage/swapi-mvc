cb2e7f12c70034d9a52b048206a9a5e6d9eead46
Sam Page <samnpage96@gmail.com>
2023-11-28 21:43:01 -0500

    httpClient contructor added

--- SwapiMvc.Mvc/Controllers/PeopleController.cs
+++ SwapiMvc.Mvc/Controllers/PeopleController.cs
@@ -4,6 +4,11 @@ namespace SwapiMvc.Mvc.Controllers;
 
 public class PeopleController : Controller
 {
+    private readonly HttpClient _httpClient;
+    public PeopleController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient("swapi");
+    }
     public async Task<IActionResult> Index()
     {
         return View();
