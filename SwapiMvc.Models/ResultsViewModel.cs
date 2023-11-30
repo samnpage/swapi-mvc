fc0d3abdeb9e98b64a0d9be828d1b08e1fc1290c
Sam Page <samnpage96@gmail.com>
2023-11-29 20:17:37 -0500

    initial commit

---
+++ SwapiMvc.Models/ResultsViewModel.cs
@@ -0,0 +1,20 @@
+using System.Text.Json.Serialization;
+
+namespace SwapiMvc.Models;
+
+public class ResultsViewModel<TResult>
+{
+    [JsonPropertyName("count")]
+    public int Count { get; set; }
+
+    [JsonPropertyName("next")]
+    public string Next { get; set; }
+
+    [JsonPropertyName("previous")]
+    public string Previous { get; set; }
+
+    [JsonPropertyName("results")]
+    public IEnumerable<TResult> Results { get; set; }
+    public string NextPageNum => Next?.Split("?page=").LastOrDefault();
+    public string PreviousPageNum => Previous?.Split("?page=").LastOrDefault();
+}
